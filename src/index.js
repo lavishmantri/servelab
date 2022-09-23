279bd0c73584eeecad2b3cb893471179e2bea134
lavishmantri <lavmantri>
2022-09-23 15:22:55 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { token } = require('morgan');
+
+const app = express();
+
+// Logging
+app.use(morgan('dev'));
+
+// Configuration
+const PORT = 4000;
+const HOST = 'localhost';
+const API_SERVICE_URL = 'https://jupyterhub.dev.markovml.com';
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+  res.send(
+    'This is a proxy service which proxies to Billing and Account APIs.',
+  );
+});
+
+// Proxy endpoints
+app.use(
+  '/jupyterlab-proxy',
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/jupyterlab-proxy`]: '',
+    },
+  }),
+);
+app.use(
+  '/user/*',
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    ws: true,
+    () => {
+      //save to mongo
+    }
+  }),
+);
+
+
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
